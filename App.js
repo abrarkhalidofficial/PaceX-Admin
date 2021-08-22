1641143401c53dcb4412eada7396483a025e1eaa
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2021-08-22 15:17:50 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+{
+  /* <NavigationContainer></NavigationContainer> */
+}
+export default function App() {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
+}
